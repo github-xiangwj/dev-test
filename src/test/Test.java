8bf5e375465615826d35f5dd8f8e57bd3f35881d
xiangwanjun <1558066833@qq.com>
2018-04-18 16:47:01 +0800

    first commit

---
+++ src/test/Test.java
@@ -0,0 +1,7 @@
+package test;
+
+public class Test {
+public static void main(String[] args) {
+	System.out.println(Float.valueOf("14.4").intValue());
+}
+}
---
+++ src/test/User.java
@@ -0,0 +1,15 @@
+package test;
+
+public class User {
+
+	private String userId ;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+}
